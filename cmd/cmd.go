897f4187de31e64805b0c4c60ea0ba63f5604035
钱志浩 <qzh6662@gmail.com>
2020-01-09 00:23:30 +0800

    add -o flag

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -1,7 +1,33 @@
 package cmd
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
+
+var (
+	outputFile  = os.Stdout
+	errorOutput = os.Stderr
+)
+
+func OutputFile(f *os.File) {
+	outputFile = f
+}
 
 func PrintN(node FileNode) {
-	fmt.Println(node.String())
+	if _, err := fmt.Fprintln(outputFile, node.String()); err != nil {
+		PrintError(err)
+		os.Exit(-1)
+	}
+}
+
+func PrintError(err error, a ...interface{}) {
+	fmt.Fprintln(errorOutput, err, a)
+}
+
+func Println(a ...interface{}) {
+	if _, err := fmt.Fprintln(outputFile, a); err != nil {
+		PrintError(err)
+		os.Exit(-1)
+	}
 }
--- main.go
+++ main.go
@@ -2,23 +2,46 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"os"
+	"tree/cmd"
 	"tree/dir"
 )
 
 var flagAll = flag.Bool("a", false, "List all files")
 var flagDepth = flag.Int("depth", 0, "Max depth")
 var flagPath = flag.String("p", ".", "Working path, default is current path")
+var flagOutput = flag.String("o", "", "Output file, default to console")
 
 func main() {
 	flag.Parse()
 	dir.FlagAll = *flagAll
 	dir.FlagDepth = *flagDepth
-	fmt.Println(*flagPath)
+	if len(*flagOutput) > 0 {
+		if _, err := os.Stat(*flagOutput); err == nil {
+			file, err := os.Open(*flagOutput)
+			if err != nil {
+				cmd.PrintError(err)
+				os.Exit(-1)
+			}
+			cmd.OutputFile(file)
+		} else if os.IsNotExist(err) {
+			file, err := os.Create(*flagOutput)
+			if err != nil {
+				cmd.PrintError(err)
+				os.Exit(-1)
+			}
+			cmd.OutputFile(file)
+		} else {
+			if err != nil {
+				cmd.PrintError(err)
+				os.Exit(-1)
+			}
+		}
+	}
+	cmd.Println(*flagPath)
 	err := dir.Visit(*flagPath)
 	if err != nil {
-		fmt.Println(err)
+		cmd.PrintError(err)
 		os.Exit(-1)
 	}
 }
